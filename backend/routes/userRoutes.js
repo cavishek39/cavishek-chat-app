e16991b43be387c09ff90d8a2138c75cb5a99fcb
Avishek Chatterjee <cavishek39@gmail.com>
2022-09-04 11:56:34 +0530

    chat routes and chat controllers developed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,10 +4,11 @@ const {
   authUser,
   allUsers,
 } = require('../controllers/userControllers')
+const { protect } = require('../middleware/auth')
 
 const router = express.Router()
 
-router.route('/').post(registerUser).get(allUsers)
+router.route('/').post(registerUser).get(protect, allUsers)
 router.post('/login', authUser)
 
 module.exports = router
