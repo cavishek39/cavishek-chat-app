145dcb524430ef0914200a4005bdbc09051c8b9e
Avishek Chatterjee <cavishek39@gmail.com>
2022-09-08 09:07:28 +0530

    prod unnecessary files de;l

--- frontend/src/secrets.ts
+++
@@ -1,4 +0,0 @@
-export const IMAGE_UPLOAD_URL =
-  'https://api.cloudinary.com/v1_1/cavishek39/image/upload'
-export const UPLOAD_PRESET = 'Groupper'
-export const CLOUD_NAME = 'cavishek39'
--- frontend/src/types/user.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-  name: string
-  email: string
-  password: string
-  profileAvatar: string
-  timestamps: string
-  token: string
-  _id: string
-}
