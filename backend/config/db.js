e15c8268a21a2f0e229298482f7e3346f561cb08
dev-avishek <avishekchatterjee@tractorjunction.com>
2023-03-08 20:43:10 +0530

    BE bug resolved

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,6 +7,7 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      connectTimeoutMS: 5000,
     })
     console.log('MONGO DB connected ', conn.connection.host)
   } catch (error) {
--- backend/index.js
+++ backend/index.js
@@ -55,13 +55,13 @@ const server = app.listen(PORT, () =>
 const io = require('socket.io')(server, {
   pingTimeout: 60000,
   cors: {
-    origin: 'http://127.0.0.1:3000/',
+    origin: 'http://localhost:3000',
     // credentials: true,
   },
 })
 
 io.on('connection', (socket) => {
-  console.log('connected to socket.io')
+  console.log('Connected to socket.io')
   socket.on('setup', (userData) => {
     socket.join(userData._id)
     socket.emit('connected')
@@ -69,23 +69,22 @@ io.on('connection', (socket) => {
 
   socket.on('join chat', (room) => {
     socket.join(room)
-    console.log('user joined the room ', room)
+    console.log('User Joined Room: ' + room)
   })
+  socket.on('typing', (room) => socket.in(room).emit('typing'))
+  socket.on('stop typing', (room) => socket.in(room).emit('stop typing'))
 
-  socket.on('new message', (newMessageReceived) => {
-    const chat = newMessageReceived.chat
+  socket.on('new message', (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat
 
-    if (!chats.users) return console.log('No user there inside the chat')
+    if (!chat.users) return console.log('chat.users not defined')
+
     chat.users.forEach((user) => {
-      if (user._id === newMessageReceived.sender._id) return
+      if (user._id == newMessageRecieved.sender._id) return
 
-      socket.in(user._id).emit('message received', newMessageReceived)
+      socket.in(user._id).emit('message recieved', newMessageRecieved)
     })
   })
-
-  socket.on('typing', (room) => socket.in(room).emit('typing'))
-
-  socket.on('stop typing', (room) => socket.in(room).emit('stop typing'))
 
   socket.off('setup', () => {
     console.log('USER DISCONNECTED')
