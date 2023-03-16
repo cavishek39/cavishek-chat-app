b2fd2fc862074a95008b358ffc9016161b0f57a5
dev-avishek <avishekchatterjee@tractorjunction.com>
2023-03-16 17:24:38 +0530

    code refactored

--- backend/config/generateToken.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('jsonwebtoken')
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_TOKEN, { expiresIn: '30d' })
-}
-
-module.exports = generateToken
--- backend/constants/constants.js
+++
@@ -1,3 +0,0 @@
-const DEFAULT_AVATAR = 'https://i.postimg.cc/KvzQLfrM/circle-cropped.png'
-
-module.exports = { DEFAULT_AVATAR }
